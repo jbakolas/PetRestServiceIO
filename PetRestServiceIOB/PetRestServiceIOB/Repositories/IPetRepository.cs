362c6296657095319cf1fb86b1ad139af31c0d89
Giannis Bakolas <jbakolas@gmail.com>
2016-07-05 03:46:00 +0300

    Implementation of the Repository pattern

---
+++ PetRestServiceIOB/PetRestServiceIOB/Repositories/IPetRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using PetRestServiceIOB.Models;
+
+namespace PetRestServiceIOB.Repositories
+{
+    public interface IPetRepository : IDisposable
+    {
+        IEnumerable<Pet> GetPets();
+        Pet Find(int petId);
+        IEnumerable<Pet> GetPetsByOwnerId(int ownerId);
+        IEnumerable<Pet> GetPetByAge(int nn);
+        void CreateNewPet(Pet pet);
+        //void DeletePet(int petId);
+        void UpdateExistingPet(Pet pet);
+        void Save();
+    }
+}
---
+++ PetRestServiceIOB/PetRestServiceIOB/Repositories/PetRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using PetRestServiceIOB.Models;
+
+//implementation of the Pet specific repository in order to create the quering methods
+namespace PetRestServiceIOB.Repositories
+{
+    public class PetRepository : IPetRepository, IDisposable
+    {
+        private PetRestServiceIOBContext context;
+
+        public PetRepository(PetRestServiceIOBContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Pet> GetPets()
+        {
+            return context.Pets.ToList();
+        }
+
+        public Pet Find(int id)
+        {
+            return context.Pets.Find(id);
+        }
+        
+        public IEnumerable<Pet> GetPetsByOwnerId(int ownerid)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public IEnumerable<Pet> GetPetByAge(int nn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateNewPet(Pet pet)
+        {
+            context.Pets.Add(pet);
+        }
+
+        public void DeletePet(int petId)
+        {
+            Pet pet = context.Pets.Find(petId);
+            context.Pets.Remove(pet);
+        }
+
+        public void UpdateExistingPet(Pet pet)
+        {
+            context.Entry(pet).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            context?.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
